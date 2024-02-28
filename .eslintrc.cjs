24fe0914a8cc1b80e09eba8931f76e1fa13641b8
Rocket201 <alejandrocorroto99@gmail.com>
2024-02-28 08:41:35 +0100

    cree las bases del proyecto

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -13,6 +13,7 @@ module.exports = {
   plugins: ['react-refresh'],
   rules: {
     'react/jsx-no-target-blank': 'off',
+    'react/prop-types': 'off',  // Disable prop-types as we use TypeScript for type checking
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
